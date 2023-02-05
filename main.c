050f332acda8797f89b39d361ca2789dc7df8d40
Durdiev Dzhuma <dzhumashop@gmail.com>
2023-02-05 09:45:06 +0500

    Функция првоерки мак-адреса

--- main.c
+++ main.c
@@ -4,44 +4,30 @@
 #include <unistd.h>
 extern char *optarg;
 void announce(void);
+int valid_mac(char *m);
 
 int main(int argc, char* argv[]){
-    /*Описание функций пользователю*/
-    if (argc == 1){
-        announce();
+    
+    if (argc == 1){ //команда без аргументов
+        announce(); 
         return 0;
     }
 
-    char *opts = "m:"; // доступные опции, каждая принимает аргумент
-    char *a; // тут храним мак - адрес
+    const char *opts = "m:"; // доступные опции, каждая принимает аргумент
+    char *mac_addr; // тут храним мак - адрес
     int opt; // каждая следующая опция попадает сюда
     while((opt = getopt(argc, argv, opts)) != -1) { // вызываем getopt пока она не вернет -1
         switch(opt) {
             case 'm': // если опция -a, преобразуем строку с аргументом в число
-                a = optarg;
-                printf("Мак-адрес для проверки: %s\n",a);//printf("");
+                mac_addr = optarg;
+                printf("Мак-адрес для проверки: %s\n",mac_addr);//printf("");
                 break;
             default:
                 printf("Неизвестный тест\n");
                 return 1; //неизвестный тест    
         }
     }
-    int i=0;
-    while(a[i] != '\0'){    
-        if((i+1)%3){
-            if( !(48<=a[i] && a[i]<=57 || 'A'<=a[i] && a[i]<='F' || 'a'<=a[i] && a[i]<='f') ){
-                printf("Некорректный MAC-адрес: недопустимые символы. Символ № %d.\n", i+1);
-                return 2; //недопустимые символы
-            }           
-        }
-        i++;
-    }
-    if(i != 17){
-        printf("Некорректный MAC-адрес: несоответствие числа символов\n");
-        return 2; // нехватка данных
-    } 
-    printf("Корректный MAC-адрес\n");
-    return 0;
+    return valid_mac(mac_addr);
 }
 /**
  * Приветствие пользователя. Описание функционала.
@@ -52,3 +38,43 @@ void announce (void){
     printf("Пример команды: $ test -m 01:02:03:AA:BB:CC\n");
     //return 0;
 }
+/**
+ *  Проверка корректности MAC-адреса. 0 - корректный. 1 - некорректный.
+*/
+int valid_mac(char *m){
+    int i=0;
+    int n=0; //число адреса
+    while(m[i] != '\0'){  
+        n = m[i]; 
+        if((i+1)%3){
+            if(!(48<=n && n<=57 || 'A'<=n && n<='F' || 'a'<=n && n<='f')){
+                printf("Некорректный MAC-адрес: недопустимые символы. Символ № %d.\n", i+1);
+                return 2; //недопустимые символы
+            }           
+        }
+        i++;
+    }
+    if(i != 17){
+            printf("Некорректный MAC-адрес: несоответствие числа символов\n");
+            return 2; // нехватка данных
+        }
+    printf("Корректный MAC-адрес\n");
+}
+
+/**
+ * Чтение команды. Проверка опции. Возврат MAC-адреса.
+*/
+/* void read_command(argc, argv, opts){
+    int opt; // каждая следующая опция попадает сюда
+    while((opt = getopt(argc, argv, opts)) != -1) { // вызываем getopt пока она не вернет -1
+        switch(opt) {
+            case 'm': // если опция -a, преобразуем строку с аргументом в число                
+                printf("Мак-адрес для проверки: %s\n",optarg);//printf("");
+                return(optarg);
+                break;
+            default:
+                printf("Неизвестный тест\n");
+                return 1; //неизвестный тест    
+        }
+    }
+} */
